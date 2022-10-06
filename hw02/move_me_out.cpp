7e34524bf0fa53f77672b6c32e5ee66201c08439
Evgenii Mikhailov <evmikhaylov@gmail.com>
2022-10-06 15:07:02 +0200

    Complete HW02

---
+++ hw02/move_me_out.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Evgenii Mikhailov on 06.10.22.
+//
+
+#include "move_me_out.h"
+
+#include <iostream>
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
