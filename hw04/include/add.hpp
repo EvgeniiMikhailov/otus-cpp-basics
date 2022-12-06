65012700b8108af6bf48a32c19bc0fbef23e833a
Evgenii Mikhailov <evmikhaylov@gmail.com>
2022-12-06 20:18:32 +0100

    Complete HW04

---
+++ hw04/include/add.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include "astnode.hpp"
+
+class Add : public ASTNode {
+  public:
+    Add(ASTNode *lhs, ASTNode *rhs)
+        : ASTNode("+", lhs, rhs) {}
+};
---
+++ hw04/include/sub.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include "astnode.hpp"
+
+class Sub : public ASTNode {
+  public:
+    Sub(ASTNode *lhs, ASTNode *rhs)
+        : ASTNode("-", lhs, rhs) {}
+};
